5d6bc21b9ba0228d9df395883aea6bc46bde7376
Alexander Aeppli <26223876+alexanderaeppli@users.noreply.github.com>
2021-01-03 22:42:40 +0100

    mix fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,4 @@
 let mix = require('laravel-mix');
 
 mix.js('assets/js/app.js', '/').setPublicPath('/');
-mix.sass('assets/scss/app.scss', 'css');
-mix.minify('app.css');
-mix.minify('app.js');
+mix.sass('assets/scss/app.scss', '/');
