616fb22a546d94f7548faf48ad145fb6d2e2dce9
Alexander Aeppli <26223876+alexanderaeppli@users.noreply.github.com>
2021-01-03 23:50:04 +0100

    todo list working

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,24 @@ const Vue = require('vue/dist/vue.esm-bundler.js')
 const app = Vue.createApp({
     data() {
         return {
-            title: 'Todo List'
+            title: 'Todo List',
+            newTodo: '',
+            todos: ['Wash clouths'],
+        }
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodo.length <= 0) {
+                alert('You cant add empty entries')
+                return
+            }
+            this.todos.push(this.newTodo)
+            this.newTodo = ''
+        },
+        removeTodo(index) {
+            if (this.todos.length >= 0) {
+                this.todos.splice(index, 1)
+            }
         }
     }
 })
