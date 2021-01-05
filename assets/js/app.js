51bb33581bbc73e822f9fd5c2e5c37b612332787
Alexander Aeppli <26223876+alexanderaeppli@users.noreply.github.com>
2021-01-05 22:18:44 +0100

    animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,42 @@
 const Vue = require('vue/dist/vue.esm-bundler.js')
+import _ from 'lodash'
 
 const app = Vue.createApp({
     data() {
         return {
-            newTodo: '',
-            todos: ['Wash clouths'],
+            newTodoField: '',
+            todos: [],
+            todoKey: 0,
         }
     },
     methods: {
+        createTodo(content) {
+            let newTodo = {
+                content: content,
+                key: this.todoKey,
+            }
+
+            this.todoKey++
+
+            return newTodo
+        },
         addTodo() {
-            if (this.newTodo.length <= 0) {
+            if (this.newTodoField.length <= 0) {
                 alert('You cant add empty entries')
                 return
             }
-            this.todos.push(this.newTodo)
-            this.newTodo = ''
+            let newTodo = this.createTodo(this.newTodoField)
+            this.todos.push(newTodo)
+            this.newTodoField = ''
         },
         removeTodo(index) {
             if (this.todos.length >= 0) {
                 this.todos.splice(index, 1)
             }
+        },
+
+        shuffle() {
+            this.todos = _.shuffle(this.todos)
         }
     },
 })
