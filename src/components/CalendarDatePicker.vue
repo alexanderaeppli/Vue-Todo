bae73dac77a276046869880487096a3d9f655b38
Alexander Aeppli <26223876+alexanderaeppli@users.noreply.github.com>
2021-01-16 17:04:36 +0100

    styling

--- src/components/CalendarDatePicker.vue
+++ src/components/CalendarDatePicker.vue
@@ -1,30 +1,44 @@
 <template>
-  <fieldset>
-    <select name="month" id="" v-model="selectedMonth">
-      <option v-for="(month, index) in months" :key="month" :value="index">
-        {{ month }}
-      </option>
-    </select>
-    <select name="year" id="" v-model="selectedYear">
-      <option
-        v-for="i in numberOfYears"
-        :key="i"
-        :value="startingYear + (i - 1)"
-      >
-        {{ startingYear + (i - 1) }}
-      </option>
-    </select>
-  </fieldset>
-  <div class="calendar">
-    <div
-      class="calendar__date--title"
-      v-for="(dayString, index) in dayStrings"
-      :key="index"
-    >
-      {{ dayString }}
+  <div class="datepicker">
+    <div class="datepicker__header">
+      <select name="month" id="" v-model="selectedMonth">
+        <option v-for="(month, index) in months" :key="month" :value="index">
+          {{ month }}
+        </option>
+      </select>
+      <select name="year" id="" v-model="selectedYear">
+        <option
+          v-for="i in numberOfYears"
+          :key="i"
+          :value="startingYear + (i - 1)"
+        >
+          {{ startingYear + (i - 1) }}
+        </option>
+      </select>
     </div>
-    <div class="calendar__date" v-for="day in month" :key="day">
-      {{ day.getDate() }}
+    <div class="calendar">
+      <div class="calendar__inner">
+        <div
+          class="calendar__date--title"
+          v-for="(dayString, index) in dayStrings"
+          :key="index"
+        >
+          <span>{{ dayString }}</span>
+        </div>
+        <div
+          class="calendar__date"
+          :class="{
+            'calendar__date--today': today.valueOf() === day.valueOf(),
+          }"
+          v-for="day in month"
+          :key="day.valueOf()"
+        >
+          <span>{{ day.getDate() }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="datepicker__footer">
+      <button class="datepicker__button--primary">Done</button>
     </div>
   </div>
 </template>
@@ -51,11 +65,19 @@ export default {
       startingYear: 2010,
       numberOfYears: 20,
       selectedDay: 1,
-      selectedMonth: 3,
+      selectedMonth: 1,
       selectedYear: 2021,
     };
   },
-  methods: {},
+  methods: {
+    markToday() {},
+    clearTime(date) {
+      date.setHours(0);
+      date.setMinutes(0);
+      date.setSeconds(0);
+      date.setMilliseconds(0);
+    },
+  },
   computed: {
     month() {
       let days = [];
@@ -68,29 +90,60 @@ export default {
       for (let i = 0; i < 35; i++) {
         let newStartDate = new Date(startDateString);
         let date = new Date(newStartDate.setDate(newStartDate.getDate() + i));
+        //this.clearTime(date);
         days[i] = date;
       }
 
       return days;
+    },
+    today() {
+      let today = new Date();
+      this.clearTime(today);
+      return today;
     },
   },
 };
 </script>
 
 <style lang="scss">
+.datepicker {
+  background-color: rgb(28, 8, 46);
+  max-width: 300px;
+  font-family: 'Source Sans', sans-serif;
+  border-radius: 10px;
+
+  &__header,
+  &__footer {
+    padding: 15px;
+  }
+}
+
 .calendar {
-  display: grid;
-  grid-template: repeat(6, 1fr) / repeat(7, 1fr);
-  width: 100%;
-  max-width: 250px;
-  height: 250px / 7 * 6;
-  justify-items: center;
-  align-items: center;
+  margin: 0 15px;
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.25);
+
+  &__inner {
+    display: grid;
+    grid-template: repeat(6, 1fr) / repeat(7, 1fr);
+    width: 100%;
+    height: 300px / 7 * 6;
+    justify-items: center;
+    align-items: center;
+    color: rgb(255, 255, 255);
+  }
 
   &__date {
     user-select: none;
-    height: 1em;
-    width: 1em;
+    height: 2em;
+    width: 2em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &--today {
+      border: black 2px solid;
+    }
   }
 }
 </style>
